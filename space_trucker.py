3d43fb230932058fea7be9295b0fe35d31a2c4c6
1fth3n3ls3 <davidtcc@gmail.com>
2020-07-18 21:53:08 +0200

    player control

--- space_trucker.py
+++ space_trucker.py
@@ -10,20 +10,54 @@ from pygame.locals import (
     K_ESCAPE
 )
 
+# CONSTANTS 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 run = True
 
-screen.fill((255, 255, 0))
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((64, 16))
+        self.surf.fill((192, 192, 192))
+        self.rect = self.surf.get_rect()
+    
+    def update(self, keys_pressed):
+        if self.rect.bottom < SCREEN_HEIGHT and keys_pressed[K_DOWN]:
+            self.rect.move_ip(0, 4)
+
+        if self.rect.top > 0 and keys_pressed[K_UP]:
+            self.rect.move_ip(0, -4)
+
+        if self.rect.left > 0 and keys_pressed[K_LEFT]:
+            self.rect.move_ip(-4, 0)
+
+        if self.rect.right < SCREEN_WIDTH and keys_pressed[K_RIGHT]:
+            self.rect.move_ip(4, 0)
+
+        
+    
+player = Player()
+
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             run = False
 
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            run = False
 
-    pygame.draw.circle(screen, (255, 0, 255), (320, 240), 64)
+    player.update(pygame.key.get_pressed())
+
+
+    screen.fill((0, 0, 50))
+    screen.blit(player.surf, player.rect)
+    
     pygame.display.flip()
 
     
