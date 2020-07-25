c09a3b1deb461c44418dd8762f1b51768ad5291b
1fth3n3ls3 <davidtcc@gmail.com>
2020-07-25 08:10:38 +0200

    enemies implementation

--- space_trucker.py
+++ space_trucker.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 from pygame.locals import (
     KEYDOWN,
@@ -17,6 +18,9 @@ SCREEN_HEIGHT = 480
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ADDENEMY = pygame.USEREVENT + 1 # create new userevent
+pygame.time.set_timer(ADDENEMY, 250)
 
 run = True
 
@@ -40,9 +44,28 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right < SCREEN_WIDTH and keys_pressed[K_RIGHT]:
             self.rect.move_ip(4, 0)
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((16, 4))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center = (random.randint(SCREEN_WIDTH + 16, SCREEN_WIDTH + 128),
+                      random.randint(0, SCREEN_HEIGHT))
+        )
+        self.speed = random.randint(4, 32)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0: self.kill()
+
         
     
 player = Player()
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 while run:
     for event in pygame.event.get():
@@ -52,11 +75,19 @@ while run:
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             run = False
 
+        if event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     player.update(pygame.key.get_pressed())
+
+    enemies.update()
 
 
     screen.fill((0, 0, 50))
-    screen.blit(player.surf, player.rect)
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
     
     pygame.display.flip()
 
